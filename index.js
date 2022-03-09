047b0771ee464fd9390ac4075e7273b8015eae00
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-09 12:46:48 -0600

    add readme file update

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-// const fs = require('fs')
-// const path = require('path')
-// // path = 'Files'
-// fs.readdir('Files', (err, files) => {
-//     if (err) {
-//         console.log(err)
-//     }
-
-//     const filteredFiles = files.filter((file) => { return file.match(/.js/) })
-//     console.log(filteredFiles)
-// })
-
 var findInFiles = require('find-in-files');
+var fs = require('fs');
+const readmeFile = 'README.md'
+let TODOcounter = 0;
 
 findInFiles
     .find({ term: /\/\/\s?todo/, flags: 'ig' }, 'Test', '.js$')
     .then(function (results) {
-        let TODOcounter = 0;
+
+        console.log('Searching for TODOs\n')
         for (var result in results) {
             var res = results[result];
             console.log(
@@ -30,8 +22,19 @@ findInFiles
 
             TODOcounter += res.count;
         }
-        console.log('Todo counter: ' + TODOcounter);
+        console.log('\nTotal found Todos: ' + TODOcounter);
+
+
+        fs.readFile(readmeFile, 'utf-8', function (err, data) {
+            if (err) throw err;
+
+            // find and replace todo counter in README file
+            var newValue = data.replace(/current\stodo\scounter:\s\d/i, `Current TODO counter: ${TODOcounter}`);
+
+            console.log(`\nUpdating ${readmeFile} file with updated todo count of ${TODOcounter}\n`)
+            fs.writeFile(readmeFile, newValue, 'utf-8', function (err, data) {
+                if (err) throw err;
+                console.log('TODO counter updated!');
+            })
+        })
     });
-
-
-  // TODO read README.md, find and replace TODO counter
