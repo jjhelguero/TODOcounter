11509862b5107a21aeeeb2d7de05774fc94450b9
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-09 13:57:56 -0600

    small updates

--- src/addOneMoreTodoToFiles.js
+++ src/addOneMoreTodoToFiles.js
@@ -1,25 +1,25 @@
 var fs = require('fs');
-const directory = 'Test'
-const flag = 'a+'
-const appendedComment = '\n// TODO valid todo'
+const directory = 'Test';
+const flag = 'a+';
+const appendedComment = '\n// TODO valid todo';
 
 function isJSFile(file) {
-    return file.indexOf('.js') >= 0
+  return file.indexOf('.js') >= 0;
 }
 
 fs.readdir(directory, function (err, filesArray) {
-    if (err) throw err;
+  if (err) throw err;
 
-    console.log(`\nFiles in ${directory} to append '${appendedComment}'\n`)
-    filesArray
-        .filter(isJSFile) // filter by .js files only
-        .forEach(file => {
-            const filePath = directory + '/' + file
-            // TODO console logs are out of sync
-            console.log(`Adding valid todo comment to ${filePath} file`)
-            fs.writeFile(filePath, appendedComment, { flag }, function (err, data) {
-                if (err) throw err;
-                console.log('Valid todo comment appended!');
-            })
-        })
-})
+  console.log(`\nFiles in ${directory} to append '${appendedComment}'\n`);
+  filesArray
+    .filter(isJSFile) // filter by .js files only
+    .forEach((file) => {
+      const filePath = directory + '/' + file;
+      // TODO console logs are out of sync
+      console.log(`Adding valid todo comment to ${filePath} file`);
+      fs.writeFile(filePath, appendedComment, { flag }, function (err, data) {
+        if (err) throw err;
+        console.log('Valid todo comment appended!');
+      });
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,48 +1,52 @@
 var findInFiles = require('find-in-files');
 var fs = require('fs');
-const readmeFile = 'README.md'
+const readmeFile = 'README.md';
 let TODOcounter = 0;
 
 findInFiles
-    .find({ term: /\/\/\s?todo/, flags: 'ig' }, 'Test', '.js$')
-    .then(function (results) {
+  .find({ term: /\/\/\s?todo/, flags: 'ig' }, 'Test', '.js$')
+  .then(function (results) {
+    console.log('Searching for TODOs\n');
+    for (var result in results) {
+      var res = results[result];
+      console.log(
+        'found "' +
+          res.matches[0] +
+          '" ' +
+          res.count +
+          ' times in "' +
+          result +
+          '"',
+      );
 
-        console.log('Searching for TODOs\n')
-        for (var result in results) {
-            var res = results[result];
-            console.log(
-                'found "' +
-                res.matches[0] +
-                '" ' +
-                res.count +
-                ' times in "' +
-                result +
-                '"',
-            );
+      TODOcounter += res.count;
+    }
+    console.log('\nTotal found Todos: ' + TODOcounter);
 
-            TODOcounter += res.count;
-        }
-        console.log('\nTotal found Todos: ' + TODOcounter);
+    fs.readFile(readmeFile, 'utf-8', function (err, data) {
+      if (err) throw err;
 
+      const readMeTodoCounter = data.match(
+        /current\stodo\scounter:\s(?<todocounter>\d)/,
+      )?.groups?.todocounter;
 
-        fs.readFile(readmeFile, 'utf-8', function (err, data) {
-            if (err) throw err;
+      // only update todo counter if current README.md todo DOES NOT match todos found
+      if (readMeTodoCounter == TODOcounter) {
+        // find and replace todo counter in README file
+        var newValue = data.replace(
+          /current\stodo\scounter:\s\d/i,
+          `Current TODO counter: ${TODOcounter}`,
+        );
 
-            const readMeTodoCounter = data.match(/current\stodo\scounter:\s(?<todocounter>\d)/)?.groups?.todocounter
-
-            // only update todo counter if current README.md todo DOES NOT match todos found
-            if (readMeTodoCounter == TODOcounter) {
-                // find and replace todo counter in README file
-                var newValue = data.replace(/current\stodo\scounter:\s\d/i, `Current TODO counter: ${TODOcounter}`);
-
-                console.log(`\nUpdating ${readmeFile} file with updated todo count of ${TODOcounter}\n`)
-                fs.writeFile(readmeFile, newValue, 'utf-8', function (err, data) {
-                    if (err) throw err;
-                    console.log('TODO counter updated!\n');
-                })
-            } else {
-                console.log('\nNo changes in TODO counter.')
-            }
-
-        })
+        console.log(
+          `\nUpdating ${readmeFile} file with updated todo count of ${TODOcounter}\n`,
+        );
+        fs.writeFile(readmeFile, newValue, 'utf-8', function (err, data) {
+          if (err) throw err;
+          console.log('TODO counter updated!\n');
+        });
+      } else {
+        console.log('\nNo changes in TODO counter.');
+      }
     });
+  });
