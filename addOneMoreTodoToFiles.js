ca26d71f9910776516f8170181a276f900a3ac9d
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-09 13:32:14 -0600

    add file to add todo to Test/*.js files

---
+++ addOneMoreTodoToFiles.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+const directory = 'Test'
+const flag = 'a+'
+const appendedComment = '\n// TODO valid todo'
+
+function isJSFile(file) {
+    return file.indexOf('.js') >= 0
+}
+
+fs.readdir(directory, function (err, filesArray) {
+    if (err) throw err;
+
+    console.log(`\nFiles in ${directory} to append '${appendedComment}'\n`)
+    filesArray
+        .filter(isJSFile) // filter by .js files only
+        .forEach(file => {
+            const filePath = directory + '/' + file
+            console.log(`Adding valid todo comment to ${file} file`)
+            fs.writeFile(filePath, appendedComment, { flag }, function (err, data) {
+                if (err) throw err;
+                console.log('Valid todo comment appended!');
+            })
+        })
+})
