d4acffc8e5645250449d4032a256a8dc8e9ee1d8
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-09 17:02:33 -0600

    refactor

--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
 var findInFiles = require('find-in-files');
 var fs = require('fs');
+
 const readmeFile = 'README.md';
+const directory = 'test'
+const fileFilter = '.js$'
+const term = /\/\/\s?todo/
+const flags = 'ig'
+
 let TODOcounter = 0;
 
 findInFiles
-  .find({ term: /\/\/\s?todo/, flags: 'ig' }, 'Test', '.js$')
+  .find({ term, flags }, directory, fileFilter)
   .then(function (results) {
     console.log('Searching for TODOs\n');
     for (var result in results) {
       var res = results[result];
       console.log(
         'found "' +
-          res.matches[0] +
-          '" ' +
-          res.count +
-          ' times in "' +
-          result +
-          '"',
+        res.matches[0] +
+        '" ' +
+        res.count +
+        ' times in "' +
+        result +
+        '"',
       );
 
       TODOcounter += res.count;
     }
-    console.log('\nTotal found Todos: ' + TODOcounter);
 
     fs.readFile(readmeFile, 'utf-8', function (err, data) {
       if (err) throw err;
 
       const readMeTodoCounter = data.match(
-        /current\stodo\scounter:\s(?<todocounter>\d)/,
-      )?.groups?.todocounter;
+        /current\stodo\scounter:\s(?<todocounter>\d)/i,
+      )?.groups?.todocounter
+
+      console.log(`\nFound todo count: ${TODOcounter}\nCurrent todo count(README.md): ${readMeTodoCounter}`)
 
       // only update todo counter if current README.md todo DOES NOT match todos found
-      if (readMeTodoCounter == TODOcounter) {
+      if (~~readMeTodoCounter == TODOcounter) {
         // find and replace todo counter in README file
         var newValue = data.replace(
           /current\stodo\scounter:\s\d/i,
