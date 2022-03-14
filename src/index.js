05d1870969fa8446a5cedd8b5a08eded22d5d817
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-14 13:48:28 -0500

    replace console.log with debug

--- src/index.js
+++ src/index.js
@@ -1,60 +1,65 @@
-var findInFiles = require('find-in-files');
-var fs = require('fs');
+const findInFiles = require('find-in-files')
+const fs = require('fs')
+const la = require('lazy-ass')
+const debug = require('debug')('findTodos')
 
-const readmeFile = 'README.md';
-const directory = 'test'
-const fileFilter = '.js$'
-const term = /\/\/\s?todo\s? \w+/
-const flags = 'ig'
-const currentTodoRegex = /current\stodo\scounter:\s(?<todocounter>\d+)/i
+/**
+ * Finds todo comments in file(s) and returns the total count
+ * @param {String} fileFilter
+ * @param {String} searchDirectory
+ * @returns {Number} total todo count
+ */
 
-let TODOcounter = 0;
+function findTodos(fileFilter, searchDirectory) {
+  la(is.unemptyString(fileFilter), 'expect file filter', fileFilter)
+  la(is.unemptyString(searchDirectory), 'expect search directory', searchDirectory)
 
-findInFiles
-  .find({ term, flags }, directory, fileFilter)
-  .then(function (results) {
-    console.log('Searching for TODOs\n');
-    for (var result in results) {
-      var res = results[result];
-      console.log(
-        'found "' +
-        res.matches[0] +
-        '" ' +
-        res.count +
-        ' times in "' +
-        result +
-        '"',
-      );
+  const readmeFile = 'README.md'
+  const term = /\/\/\s?todo\s? \w+/
+  const flags = 'ig'
+  const currentTodoRegex = /current\stodo\scounter:\s(?<todocounter>\d+)/i
 
-      TODOcounter += res.count;
-    }
+  let TODOcounter = 0
 
-    fs.readFile(readmeFile, 'utf-8', function (err, data) {
-      if (err) throw err;
+  debug(`Searching for todos in ${searchDirectory}`)
 
-      const readMeTodoCounter = data.match(
-        currentTodoRegex,
-      )?.groups?.todocounter
+  findInFiles
+    .find({ term, flags }, searchDirectory, fileFilter)
+    .then(function (results) {
+      for (var result in results) {
+        var res = results[result]
+        debug(`found "${res.matches[0]}" ${res.count} times in "${result}"`)
 
-      console.log(`\nFound todo count: ${TODOcounter}\nCurrent todo count(README.md): ${readMeTodoCounter}`)
-
-      // only update todo counter if current README.md todo DOES NOT match todos found
-      if (~~readMeTodoCounter !== TODOcounter) {
-        // find and replace todo counter in README file
-        var newValue = data.replace(
-          currentTodoRegex,
-          `Current TODO counter: ${TODOcounter}`,
-        );
-
-        console.log(
-          `\nUpdating ${readmeFile} file with updated todo count of ${TODOcounter}\n`,
-        );
-        fs.writeFile(readmeFile, newValue, 'utf-8', function (err, data) {
-          if (err) throw err;
-          console.log('TODO counter updated!\n');
-        });
-      } else {
-        console.log('\nNo changes in TODO counter.');
+        TODOcounter += res.count
       }
-    });
-  });
+
+      fs.readFile(readmeFile, 'utf-8', function (err, data) {
+        if (err) throw err
+
+        const readMeTodoCounter = data.match(
+          currentTodoRegex,
+        )?.groups?.todocounter
+
+        debug(`Found todo count: ${TODOcounter}\nCurrent todo count(README.md): ${readMeTodoCounter}`)
+
+        // only update todo counter if current README.md todo DOES NOT match todos found
+        if (~~readMeTodoCounter !== TODOcounter) {
+          // find and replace todo counter in README file
+          var newValue = data.replace(
+            currentTodoRegex,
+            `Current TODO counter: ${TODOcounter}`,
+          )
+
+          debug(`\nUpdating ${readmeFile} file with updated todo count of ${TODOcounter}\n`)
+          fs.writeFile(readmeFile, newValue, 'utf-8', function (err, data) {
+            if (err) throw err
+            debug('TODO counter updated!')
+          })
+        } else {
+          debug('No changes in TODO counter.')
+        }
+      })
+    })
+
+}
+module.exports = { findTodos }
