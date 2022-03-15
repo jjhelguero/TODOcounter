b0d37372a7a9b4d2cb9be74722a075c65a6708be
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-15 16:50:51 -0500

    initial commit

--- src/updateReadme.js
+++ src/updateReadme.js
@@ -1,35 +1,69 @@
-function udpateReadMeTodoCounter(tcounter) {
-  const readmeFile = 'README.md';
-  const currentTodoRegex = /current\stodo\scounter:\s(?<todocounter>\d+)/i;
+const fs = require('fs')
+const debug = require('debug')('updateReadMeTodoCounter')
+var dayjs = require('dayjs')
+
+
+// const todoRowRegex = /(?<todoRow>\|<date\d+>(\d{2}\/\d{2}\/\d{2})?\|<todoCounter\d+>(\d+)?\|)/gi
+
+function extractTodoRows(readMe) {
+  debug('Extracting todo rows')
+  // const todoRowRegex = /\|<date\d+>(\d{2}\/\d{2}\/\d{2})?\|<todoCounter\d+>(\d+)?\|/gi
+  const todoRowRegex = /\|<date>(\d{2}\/\d{2}\/\d{2})?\|<todoCounter>(\d+)?\|/gi
+  const matchedRows = (readMe || '').match(todoRowRegex) || []
+  debug(`Found ${matchedRows.length} matches`)
+
+  return matchedRows
+}
+
+function updateTodoTable(rowArray, todoCount) {
+  debug('Updating todo table')
+  const d = new Date()
+  const date = dayjs(d).format('MM/DD/YY')
+  const newRow = `|<date>${date}|<todoCounter>${todoCount}|`
+  rowArray.push(newRow)
+
+  console.log(rowArray)
+
+  // rowArray.shift()
+
+  debug('Todo table updated!')
+}
+
+function updateReadMeTodoCounter(tcounter) {
+  const readmeFile = 'README.md'
 
   fs.readFile(readmeFile, 'utf-8', function (err, data) {
     if (err) throw err;
 
-    const readMeTodoCounter = data.match(currentTodoRegex)?.groups?.todocounter;
+    const todoRows = extractTodoRows(data)
 
-    console.log(
-      `Found todo count: ${tcounter}\nREADME.md todo count: ${readMeTodoCounter}`,
-    );
+    updateTodoTable(todoRows, tcounter)
 
-    // only update todo counter if current README.md todo DOES NOT match todos found
-    if (~~readMeTodoCounter !== tcounter) {
-      // find and replace todo counter in README file
-      var newValue = data.replace(
-        currentTodoRegex,
-        `Current TODO counter: ${tcounter}`,
-      );
+    // TODO update
+    // console.log({ todoRows })
+    // console.log(
+    //   `Found todo count: ${tcounter}\nREADME.md todo count: ${readMeTodoCounter}`,
+    // );
 
-      console.log(
-        `Updating ${readmeFile} file with updated todo count of ${tcounter}`,
-      );
-      fs.writeFile(readmeFile, newValue, 'utf-8', function (err, data) {
-        if (err) throw err;
-        console.log('TODO counter updated!');
-      });
-    } else {
-      console.log('No changes in TODO counter.');
-    }
-  });
+    //   // only update todo counter if current README.md todo DOES NOT match todos found
+    //   if (~~readMeTodoCounter !== tcounter) {
+    //     // find and replace todo counter in README file
+    //     var newValue = data.replace(
+    //       currentTodoRegex,
+    //       `Current TODO counter: ${tcounter}`,
+    //     );
+
+    //     console.log(
+    //       `Updating ${readmeFile} file with updated todo count of ${tcounter}`,
+    //     );
+    //     fs.writeFile(readmeFile, newValue, 'utf-8', function (err, data) {
+    //       if (err) throw err;
+    //       console.log('TODO counter updated!');
+    //     });
+    //   } else {
+    //     console.log('No changes in TODO counter.');
+    //   }
+  })
 }
 
 function searchForTodos(results) {
@@ -43,4 +77,5 @@ function searchForTodos(results) {
   udpateReadMeTodoCounter(TODOcounter);
 }
 
-module.exports = udpateReadMeTodoCounter;
+updateReadMeTodoCounter(2)
+module.exports = updateReadMeTodoCounter
