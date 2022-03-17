00270b0245da491cba769e64c501de77b7082650
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-16 22:43:21 -0500

    rmeove semicolons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-const la = require('lazy-ass');
-const searchFilesInDirectory = require('./searchFilesInDirectory');
+const la = require('lazy-ass')
+const searchFilesInDirectory = require('./searchFilesInDirectory')
 
 function findAndUpdateTodoCounter(dir, ext) {
-  la(is.unemptyString(dir), 'expect search directory', dir);
-  la(is.unemptyString(ext), 'expect file filter', ext);
+  la(is.unemptyString(dir), 'expect search directory', dir)
+  la(is.unemptyString(ext), 'expect file filter', ext)
 
-  const currentTodos = searchFilesInDirectory(dir, ext);
+  const currentTodos = searchFilesInDirectory(dir, ext)
 
   la(is.number(currentTodos), 'expect todo counter to be a number', currentTodos)
-  udpateReadMeTodoCounter(currentTodos);
+  udpateReadMeTodoCounter(currentTodos)
 }
-module.exports = { findAndUpdateTodoCounter };
+
+module.exports = { findAndUpdateTodoCounter }
