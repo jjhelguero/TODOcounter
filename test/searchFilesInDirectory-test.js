4ac94db4cc7eeb5452fbd7cd244c73a5b09f8922
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-17 16:54:24 -0500

    add tests

--- test/searchFilesInDirectory-test.js
+++ test/searchFilesInDirectory-test.js
@@ -1,7 +1,16 @@
 const test = require('ava')
-const searchFilesInDirectory = require('../src/searchFilesInDirectory')
+const getTodoCount = require('../src/searchFilesInDirectory')
+const getFilesInDirectory = require('../src/searchFilesInDirectory')
 
-test('returns number', t => {
-    const todoCount = searchFilesInDirectory('test/files', '.js')
-    t.true(typeof todoCount == 'number', 'todoCount returns a number')
+test('getFilesInDirectory returns number of files', t => {
+    t.true(typeof getFilesInDirectory('test/files', '.js') == 'number')
+})
+
+test('getFilesInDirectory should return empty string when nullish value', t => {
+    const validExt = '.js'
+    const nullDirectory = null
+    const undefinedDirectory = undefined
+    t.is(getFilesInDirectory(nullDirectory, validExt), undefined)
+    t.is(getFilesInDirectory(undefinedDirectory, validExt), undefined)
+    t.is(getFilesInDirectory('invalidDirect', validExt), undefined)
 })
