ef7d90f4c354d2cffa90108724febf43e7e612f7
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-21 14:56:06 -0500

    fix(cli): add bin reference

--- bin/count-todos.js
+++ bin/count-todos.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const debug = require('debug')('check-todo-count')
+const debug = require('debug')('count-todos')
 const la = require('lazy-ass')
 const is = require('check-more-types')
 const join = require('path').join
@@ -8,14 +8,14 @@ const join = require('path').join
 require('simple-bin-help')({
     minArguments: 2,
     packagePath: join(__dirname, '..', 'package.json'),
-    help: 'use    : countTodos <directory> <file extension>\n' +
-        'example: countTodos "tests/files" ".js"'
+    help: 'use    : count-todos <directory> <file extension>\n' +
+        'example: count-todos "tests/files" ".js"'
 })
 
 const directory = process.argv[2]
 const fileExt = process.argv.slice(3)
 debug('directory and file extension: %s - %o', directory, fileExt)
 
-const countTodos = require('..').countTodos
+const countTodos = require('../src').countTodos
 la(is.fn(countTodos), 'expected function', countTodos)
 countTodos(directory, fileExt)
