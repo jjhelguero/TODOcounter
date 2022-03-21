0ee6ad9ea9d7867b20e165b06c66742d39905949
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-21 14:45:50 -0500

    fix(cli): fix name help text

--- bin/check-todo-count.js
+++ bin/check-todo-count.js
@@ -8,8 +8,8 @@ const join = require('path').join
 require('simple-bin-help')({
     minArguments: 2,
     packagePath: join(__dirname, '..', 'package.json'),
-    help: 'use    : check-todo-count <directory> <file extension>\n' +
-        'example: check-todo-count "tests/files" ".js"'
+    help: 'use    : countTodos <directory> <file extension>\n' +
+        'example: countTodos "tests/files" ".js"'
 })
 
 const directory = process.argv[2]
