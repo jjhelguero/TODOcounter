d8dccef3176820ee51c218184ffc0eff08c4b427
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-21 22:30:36 -0500

    fix: lazy-ass

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const { lazyAss } = require('lazy-ass')
 const is = require('check-more-types')
+const check = require('check-more-types')
 const searchFilesInDirectory = require('./searchFilesInDirectory')
 const udpateReadMeTodoCounter = require('./updateReadme')
 
 function todoCounter(dir, ext) {
   lazyAss(is.unemptyString(dir), 'expect search directory', dir)
-  lazyAss(is.unemptyString(ext), 'expect file filter', ext)
+  lazyAss(check.arrayOf(check.unemptyString, ext), 'expect file filter', ext)
 
   const currentTodos = searchFilesInDirectory(dir, ext)
 
