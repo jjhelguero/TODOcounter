1ba8e6be350b93ce1cfcb1e607b4b15629fc9234
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2022-03-22 15:00:26 -0500

    fix: demo files

--- demo/subdirectory/file.js
+++ demo/subdirectory/file.js
@@ -1,8 +1,10 @@
 // TODO: this is be counted
 //todo: this is be counted
 // todo this will be counted
+function nothing() {
+    // TODO: this is be counted
+}
 
 //  todo will not be counted
 /\/\/todo will not be counted/
-"//todo will not be counted"
 // todowillnotbecounted
