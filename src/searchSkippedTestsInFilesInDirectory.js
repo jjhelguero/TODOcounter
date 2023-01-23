c3f52e24fcade088a52c7c20ad2e4fb71446d90b
jjhelguero <joshua.helguero@mercari.com>
2023-01-23 12:07:45 -0600

    chore: add searchSkippedTestsInFilesInDirectory

---
+++ src/searchSkippedTestsInFilesInDirectory.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const util = require('util')
+const {getFilesInDirectory, getCount} = require('./utils')
+const debug = require('debug')('searchTodosInFilesInDirectory')
+const skippedTestRegex = /\/{2}\s?skip(\s|:)?/
+
+/**
+ * Function to get total count of todos within files matching ext within
+ * directory(icluding subdirectories)
+ * @param {String} dir directory string
+ * @param {String} ext file extenstion string
+ * @returns {Number} returns total todo count in files
+ */
+function searchSkippedTestsInFilesInDirectory(dir, ext) {
+  if (!fs.existsSync(dir)) {
+    debug(`Specified directory: ${dir} does not exist`)
+    return
+  }
+
+  debug(`Searching for todos in ${dir}`)
+
+  const files = getFilesInDirectory(dir, ext)
+
+  debug('Files found: \n' + util.inspect(files, { maxArrayLength: null }))
+
+  return getCount(files, skippedTestRegex)
+}
+
+;(module.exports = searchSkippedTestsInFilesInDirectory)
