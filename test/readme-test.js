74684711010df593e8834f2450bd450e1362c5a8
jjhelguero <joshua.helguero@mercari.com>
2023-01-24 13:05:42 -0600

    chore: add another readme test

--- test/readme-test.js
+++ test/readme-test.js
@@ -2,9 +2,17 @@ const test = require('ava')
 const mock = require('mock-fs')
 const { extractTableFromReadme } = require('../src/readmeTableUpdates/utils')
 
-test.only('extractTableFromReadme for todoCounter returns a number', (t) => {
+test('extractTableFromReadme for todoCounter returns a number', (t) => {
     mock({ 
         'mockFile1.md': '| <date>01/01/01 | <todoCounter>1  |',
     })
     t.true(typeof extractTableFromReadme('mockFile1.md', 'todo') === 'number')
+})
+
+test('extractTableFromReadme matches 2 todo counter rows', (t) => {
+    mock({ 
+        'mockFile1.md': `| <date>01/01/01 | <todoCounter>1  |
+        | <date>02/02/02 | <todoCounter>2  |`,
+    })
+    t.true(extractTableFromReadme('mockFile1.md', 'todo') === 2)
 })
