d945221e51d9ad6f612a030352db3cca9bcb23ad
jjhelguero <joshua.helguero@mercari.com>
2023-01-24 13:35:26 -0600

    chore: add another test and refactor a bit

--- test/readme-test.js
+++ test/readme-test.js
@@ -1,12 +1,13 @@
 const test = require('ava')
 const mock = require('mock-fs')
-const { extractTableFromReadme } = require('../src/readmeTableUpdates/utils')
+const util = require('util')
+const { extractTableFromReadme, COUNT_TYPE } = require('../src/readmeTableUpdates/utils')
 
 test('extractTableFromReadme for todoCounter returns a number', (t) => {
     mock({ 
         'mockFile1.md': '| <date>01/01/01 | <todoCounter>1  |',
     })
-    t.true(typeof extractTableFromReadme('mockFile1.md', 'todo') === 'number')
+    t.true(typeof extractTableFromReadme('mockFile1.md', COUNT_TYPE.TODO) === 'number')
 })
 
 test('extractTableFromReadme matches 2 todo counter rows', (t) => {
@@ -14,5 +15,24 @@ test('extractTableFromReadme matches 2 todo counter rows', (t) => {
         'mockFile1.md': `| <date>01/01/01 | <todoCounter>1  |
         | <date>02/02/02 | <todoCounter>2  |`,
     })
-    t.true(extractTableFromReadme('mockFile1.md', 'todo') === 2)
+    t.true(extractTableFromReadme('mockFile1.md', COUNT_TYPE.TODO) === 2)
+})
+
+test('extractTableFromReadme throws error when countType does not match', (t) => {
+    mock({ 
+        'mockFile1.md': '| <date>01/01/01 | <todoCounter>1  |',
+    })
+    const incorrectType = 't'
+    const errorMessage = `${incorrectType} is not one of ${util.inspect(COUNT_TYPE)}`
+    const error = t.throws(
+        () => extractTableFromReadme('mockFile1.md', incorrectType))
+
+	t.is(error.message, errorMessage);
+})
+
+test('extractTableFromReadme throws error when file is not passed', (t) => {
+    mock({ 
+        path: {},
+    })
+    t.throws(() => extractTableFromReadme('path', COUNT_TYPE.TODO))
 })
