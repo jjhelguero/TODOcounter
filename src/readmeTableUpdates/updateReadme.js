c2de673cda66a2d15a7c584fc1e6ac7982feeaf3
jjhelguero <joshua.helguero@mercari.com>
2023-01-24 13:03:30 -0600

    chore: update maybeUpdateReadMeCounter function

--- src/readmeTableUpdates/updateReadme.js
+++ src/readmeTableUpdates/updateReadme.js
@@ -1,20 +1,28 @@
 const fs = require('fs')
 const debug = require('debug')('updateReadMeTodoCounter')
-const {extractTableFromReadme, maybeUpdateReadmeTable} = require('./utils')
+const {extractTableFromReadme, maybeUpdateReadmeTable, COUNT_TYPE} = require('./utils')
 const encoding = 'utf-8'
 
 
-
-function maybeUpdateReadMeCounter(count, countType) {
+function maybeUpdateReadMeCounter(count, type) {
   const readmeFile = 'README.md'
+  let headerString, tableHeaderTag
+  
+  if(type == COUNT_TYPE.TODO) {
+    headerString = 'Todo'
+    tableHeaderTag  = 'todoCounter'
+  } else if (type == COUNT_TYPE.SKIP){
+    headerString = 'Skipped Tests'
+    tableHeaderTag  = 'skippedTestsCounter'
+  }
 
   debug(`Reading ${readmeFile} file`)
-  fs.readFile(readmeFile, encoding, function (err, data) {
+  fs.readFileSync(readmeFile, encoding, function (err, data) {
     if (err) throw err
 
-    const todoTable = extractTableFromReadme(data)
+    const todoTable = extractTableFromReadme(data, tableHeaderTag)
 
-    maybeUpdateReadmeTable(readmeFile, data, todoTable, count, countType)
+    maybeUpdateReadmeTable(readmeFile, data, todoTable, count, tableHeaderString)
   })
 }
 
