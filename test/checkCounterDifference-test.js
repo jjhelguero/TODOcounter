40ba68b37d1ef38115e4fd09070f86659fc8d9bb
jjhelguero <joshua.helguero@mercari.com>
2023-01-24 17:21:36 -0600

    chore: remove commented code

--- test/checkCounterDifference-test.js
+++ test/checkCounterDifference-test.js
@@ -1,14 +1,3 @@
-// function checkCounterDifference(table, count, tableHeader) {
-//   const lastRow = table[table.length - 1]
-//   const lastCountRegex = new RegExp(`<${tableHeader}>(?<count>\d+)`)
-//   const latestCount = lastRow.match(lastCountRegex).groups.count
-//   debug(
-//     `Latest table todo count: ${latestCount}\nFound todo count: ${count}`,
-//   )
-
-//   return latestCount != count
-// }
-
 const test = require('ava')
 const {checkCounterDifference, COUNT_TYPE} = require('../src/readmeTableUpdates/utils')
 
