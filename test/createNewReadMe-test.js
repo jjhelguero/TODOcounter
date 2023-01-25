f511a33d26a8f92097a21c904e713be7aa806b47
jjhelguero <joshua.helguero@mercari.com>
2023-01-25 10:17:25 -0600

    chore: refactor test

--- test/createNewReadMe-test.js
+++ test/createNewReadMe-test.js
@@ -6,29 +6,20 @@ const {createNewReadMe, COUNT_TYPE} = require('../src/readmeTableUpdates/utils')
 
 test('createNewReadMe creates README Text', t => {
      mock({ 
-        'mockFile1.md': `
-        | <date>01/01/01 | <todoCounter>1  |
+        'mockFile1.md': `| <date>01/01/01 | <todoCounter>1  |
         | <date>02/02/02 | <todoCounter>2  |
         | <date>03/03/03 | <todoCounter>3  |
-        | <date>04/04/04 | <todoCounter>4  |
-        `
+        | <date>04/04/04 | <todoCounter>4  |`
     })
     const data = fs.readFileSync('mockFile1.md').toString()
-    const oldTable = [
-        '| <date>01/01/01 | <todoCounter>1  |',
-        '| <date>02/02/02 | <todoCounter>2  |',
-        '| <date>03/03/03 | <todoCounter>3  |',
-        '| <date>04/04/04 | <todoCounter>4  |'
-    ]
+    const oldTable = data.split(/\n        /)
+    const formattedOldTable = oldTable.toString().replace(/\|,/g, '|\n')
     const count = 10
     const d = new Date()
     const date = dayjs(d).format('MM/DD/YY')
     const expectedNewReadMe = `| Date | ${COUNT_TYPE.TODO.type} Count |
 | :---:| :---:|
-${oldTable[0]}
-${oldTable[1]}
-${oldTable[2]}
-${oldTable[3]}
+${formattedOldTable}
 | <date>${date} | <todoCounter>${count} |
 `
     const newTable = createNewReadMe(data, oldTable, count, COUNT_TYPE.TODO.type)
