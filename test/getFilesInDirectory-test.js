49b1753ed92b386e3acb13305e5ad08beefcb1ed
jjhelguero <joshua.helguero@mercari.com>
2023-02-06 15:29:05 -0600

    feat: update tests and code to handle array

--- test/getFilesInDirectory-test.js
+++ test/getFilesInDirectory-test.js
@@ -2,34 +2,51 @@ const test = require('ava')
 const mock = require('mock-fs')
 const { getFilesInDirectory } = require('../src/utils')
 
-test('getFilesInDirectory returns object of more than 1 file', (t) => {
+const happyPathMockDir = {
+  'mockFile1.js': '//todo\n// todo',
+  'mockFile1.ts': '//todo\n// todo',
+  'empty-dir': {},
+  sub: {
+    'mockFile2.js': '// todo',
+    'mockFile2.ts': '// todo',
+  },
+  'mockFile.java': '',
+}
+
+test('getFilesInDirectory returns an object ', (t) => {
   mock({
-    fakeDir: {
-      'mockFile1.js': '//todo\n// todo',
-      'empty-dir': {},
-      sub: {
-        'mockFile2.js': '// todo',
-      },
-      'mockFile.java': '',
-    },
+    fakeDir: happyPathMockDir,
   })
-  t.true(typeof getFilesInDirectory('fakeDir', '.js') === 'object')
-  t.true(getFilesInDirectory('fakeDir', '.js').length >= 1)
+  const actual = typeof getFilesInDirectory('fakeDir', '.js')
+  const expected = 'object'
+  t.true(actual === expected)
+})
+
+test('getFilesInDirectory returns 4 files', (t) => {
+  mock({
+    fakeDir: happyPathMockDir,
+  })
+  const actual = getFilesInDirectory('fakeDir', '[.js,.ts]').length
+  const expected = 4
+  t.true(actual == expected)
 })
 
 test('getFilesInDirectory should return empty string with null directory', (t) => {
   const validExt = '.js'
   const nullDirectory = null
-  t.is(getFilesInDirectory(nullDirectory, validExt), undefined)
+  const actual = getFilesInDirectory(nullDirectory, validExt)
+  t.is(actual, undefined)
 })
 
 test('getFilesInDirectory should return empty string with undefined directory', (t) => {
   const validExt = '.js'
   const undefinedDirectory = undefined
-  t.is(getFilesInDirectory(undefinedDirectory, validExt), undefined)
+  const actual = getFilesInDirectory(undefinedDirectory, validExt)
+  t.is(actual, undefined)
 })
 
 test('getFilesInDirectory should return empty string with invalid directory', (t) => {
   const validExt = '.js'
-  t.is(getFilesInDirectory('invalidDirect', validExt), undefined)
+  const actual = getFilesInDirectory('invalidDirect', validExt)
+  t.is(actual, undefined)
 })
