aef77dba2745333757f95e929c8c7066c36bfc0a
jjhelguero <joshua.helguero@mercari.com>
2023-02-06 15:16:17 -0600

    chore: add .ts demo files

---
+++ demo/file.ts
@@ -0,0 +1,16 @@
+// TODO: this will be counted
+//todo: this will be counted
+// todo this will be counted
+function nothingD(): void {
+  // TODO: this is be counted
+}
+
+//  todo will be counted
+;/\/\/todo will not be counted/
+// todowillnotbecounted
+// TODO add another valid todo
+
+// it.skip
+// describe.skip
+//SKIP: this will be NOT counted
+// skip  this will be NOt counted
